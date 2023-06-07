4029fbb906334f61fda0d3258c4594ff614c0277
Nguyen Le <lenguyen_d@hotmail.com>
2023-06-07 13:15:59 -0700

    regular update

--- UCSD/AlgToolBox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ UCSD/AlgToolBox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -3,6 +3,16 @@
 # @Time    : 2019/3/2 11:00 AM
 # @Author  : Nguyen Le
 
+def max_pairwise_product_loop(numbers):
+    max_product = 0
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j:
+                product = numbers[i] * numbers[j]
+                if product > max_product:
+                    max_product = product
+    return max_product
+
 def max_pairwise_product(numbers):
     #P# Find the 2 max values in the list numbers
     #P# 1st max value is the max value in the list in firstMax
@@ -16,6 +26,33 @@ def max_pairwise_product(numbers):
 
 
 if __name__ == '__main__':
+    #P# write quick tests to check the correctness of your code
+    # 1. Create a list of 10 positive random integers
+    # 2. Call the function max_pairwise_product_loop to get the max pairwise product
+    # 3. Call the function max_pairwise_product to get the max pairwise product
+    # 4. Compare the results from 2 and 3
+    # 5. If the results are different, print out the list of 10 positive random integers
+    #P# and raise an exception
+    #P# loop for 10 times
+    #P# run the test if TEST_CASE is True
+    TEST_CASE = True
+    if TEST_CASE:
+        import random
+        for i in range(10):
+            # create a list of 10 positive random integers
+            numbers = [random.randint(1, 1000) for i in range(10)]
+            # call the function max_pairwise_product_loop to get the max pairwise product
+            result1 = max_pairwise_product_loop(numbers)
+            # call the function max_pairwise_product to get the max pairwise product
+            result2 = max_pairwise_product(numbers)
+            # compare the results from 2 and 3
+            # if the results are different, print out the list of 10 positive random integers
+            # and raise an exception
+            if result1 != result2:
+                print(numbers)
+                raise Exception('Wrong answer: {} {}'.format(result1, result2))
+            else:
+                print('OK i = ', i)
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
--- UCSD/AlgToolBox/week3_greedy_algorithms/1_money_change/change.py
+++ UCSD/AlgToolBox/week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,45 @@
-# Uses python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Problem Description
+# Task. The goal in this problem is to find the minimum number of coins 
+# needed to change the input value
+# (an integer) into coins with denominations 1, 5, and 10.
+# Input Format. The input consists of a single integer 𝑚.
+# Constraints. 1 ≤𝑚 ≤103.
+# Output Format. Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
+# Sample 1.
+# Input:
+# 2
+# Output:
+# 2
+# 2 = 1 + 1.
+# Sample 2.
+# Input:
+# 28
+# Output:
+# 6
+# 28 = 10 + 10 + 5 + 1 + 1 + 1.
+# Need Help?
+# Ask a question or see the questions asked by other learners at this forum thread.
+# 3
+
 import sys
+
+#P# 1. Divide the amount by 10, 5 and 1
+#P# 2. Get the remainder
+#P# 3. Repeat the process with the remainder
+#P# 4. Add the number of times the amount was divided by 10, 5 and 1
 
 def get_change(m):
     #write your code here
-    return m
+    coins = [10, 5, 1]
+    count = 0
+    for coin in coins:
+        count += m // coin
+        m %= coin
+    return count
+
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
